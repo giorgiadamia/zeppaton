8d60fa4ec0e37b3974d15dca8304fc047e3b4bcc
Giorgi <1giorgiadamia@gmail.com>
2022-11-29 11:12:09 +0400

    added filtration

--- src/main/java/com/example/zeppaton/controller/MessageController.java
+++ src/main/java/com/example/zeppaton/controller/MessageController.java
@@ -30,4 +30,16 @@ public class MessageController {
         model.put("messages", messages);
         return "main";
     }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
+    }
 }
