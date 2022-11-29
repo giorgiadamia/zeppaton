6ee1bb5f74570051f2909e84f145791ae04ccfbf
Giorgi <1giorgiadamia@gmail.com>
2022-11-29 11:12:45 +0400

    added repo with findByTag

---
+++ src/main/java/com/example/zeppaton/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.zeppaton.repos;
+
+import com.example.zeppaton.model.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
