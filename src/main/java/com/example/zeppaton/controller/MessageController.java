6b5d33bfcf65f562f642262822260da87cacd6b5
Giorgi <1giorgiadamia@gmail.com>
2022-12-19 23:58:03 +0400

    added upload path check

--- src/main/java/com/example/zeppaton/controller/MessageController.java
+++ src/main/java/com/example/zeppaton/controller/MessageController.java
@@ -4,19 +4,25 @@ import com.example.zeppaton.model.domain.Message;
 import com.example.zeppaton.model.domain.User;
 import com.example.zeppaton.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.Map;
 
 @Controller
 public class MessageController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -43,8 +49,18 @@ public class MessageController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model) {
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile multipartFile) {
         Message message = new Message(text, tag, user);
+        if (multipartFile != null) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+//            message.setFilename();
+        }
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
