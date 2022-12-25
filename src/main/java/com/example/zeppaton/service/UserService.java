a90207089422db37e417958bcd282444c41604a1
Giorgi <1giorgiadamia@gmail.com>
2022-12-25 23:21:32 +0400

    refactor, processing in service

--- src/main/java/com/example/zeppaton/service/UserService.java
+++ src/main/java/com/example/zeppaton/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.zeppaton.service;
 
+import com.example.zeppaton.model.domain.Role;
+import com.example.zeppaton.model.domain.User;
 import com.example.zeppaton.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,14 +9,46 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.UUID;
+
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private MailSender mailSender;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    // POST request, registration
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepo.save(user);
+
+        if (!user.getEmail().isEmpty()) {
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Zeppaton. Please visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return true;
     }
 }
