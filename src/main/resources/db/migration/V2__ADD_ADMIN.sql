e609aa1d5d9ac5b79a12f57ca0739d195f1f7c99
Giorgi <1giorgiadamia@gmail.com>
2022-12-30 22:34:05 +0400

    update admin password

--- src/main/resources/db/migration/V2__ADD_ADMIN.sql
+++ src/main/resources/db/migration/V2__ADD_ADMIN.sql
@@ -1,5 +1,5 @@
 insert into zeppaton.usr (id, username, password, active)
-values (1, 'admin', 'admin', true);
+values (1, 'admin', '$2a$08$1SYXgdm7ni.7ZTp1QTyvRuFW52Wei9/x4rMuZMxlNxbByQCEqRItK', true);
 
 insert into zeppaton.user_role (user_id, roles)
 values (1, 'USER'), (1, 'ADMIN');
